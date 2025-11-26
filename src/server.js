7eba78545fbe3421cfd21ef00906b9bab7e9603e
matias lujan <lujan9814@gmail.com>
2025-11-26 02:35:15 -0300

    Fix(openapi): Fix openapi.json filename

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import morgan from 'morgan';
+
 import songRouter from './routers/song.router.js';
 import userRouter from './routers/user.router.js';
 import authRouter from './routers/auth.routers.js';
@@ -7,11 +8,23 @@ import playlistRouter from './routers/playlist.router.js';
 import notFounderHandler from './middleware/notFoundHandler.js';
 import statsRouter from './routers/stats.router.js';
 import statsExportRouter from './routers/stats.export.router.js';
+
 import swaggerUi from 'swagger-ui-express';
-import swaggerDocument from '../docs/openapi.json' assert { type: 'json' };
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 const server = express();
 const morganFormat = morgan(':method :url :status :res[content-length] - :response-time ms');
+
+//__dirname en ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+//Cargar archivo OpenAPI JSON
+const swaggerPath = path.join(__dirname, '..', 'docs', 'openapi.json');
+const swaggerRaw = fs.readFileSync(swaggerPath, 'utf8');
+const swaggerDocument = JSON.parse(swaggerRaw);
 
 server.use(express.json());
 server.use(morganFormat);
